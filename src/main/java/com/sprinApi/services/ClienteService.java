db17240880f3ad7d1779f4cf8ce4dde2a614494d
Tiago Moura <trmoura@yahoo.com.br>
2018-02-17 14:31:48 -0300

    Endpoint clientes/{id}

---
+++ src/main/java/com/sprinApi/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.sprinApi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sprinApi.domain.Cliente;
+import com.sprinApi.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+
+		Cliente Cliente = repo.findOne(id);
+
+		if (Cliente == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado para o  ID :" + id + " do Tipo: " + Cliente.class.getSimpleName());
+		}
+
+		return Cliente;
+	}
+
+}
