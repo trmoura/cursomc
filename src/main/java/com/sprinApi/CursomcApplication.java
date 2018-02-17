3a35997fb923e3b6952b4f378184b13acd5dc07f
Tiago Moura <trmoura@yahoo.com.br>
2018-02-17 13:32:09 -0300

    Estado e Cidade

--- src/main/java/com/sprinApi/CursomcApplication.java
+++ src/main/java/com/sprinApi/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.sprinApi;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sprinApi.domain.Categoria;
+import com.sprinApi.domain.Cidade;
+import com.sprinApi.domain.Estado;
 import com.sprinApi.domain.Produto;
 import com.sprinApi.repositories.CategoriaRepository;
+import com.sprinApi.repositories.CidadeRepository;
+import com.sprinApi.repositories.EstadoRepository;
 import com.sprinApi.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +26,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,5 +56,18 @@ public class CursomcApplication implements CommandLineRunner {
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
+		Estado est1 = new Estado(null, "Minas Gerais", "MG");
+		Estado est2 = new Estado(null, "São Pualo", "SP");
+
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 	}
 }
