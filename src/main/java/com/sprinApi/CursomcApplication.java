5f243fbc40b6127b1f2c1c0d69928983af904521
Tiago Moura <trmoura@yahoo.com.br>
2018-02-17 09:12:09 -0300

    Criando operação de instaciação

--- src/main/java/com/sprinApi/CursomcApplication.java
+++ src/main/java/com/sprinApi/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.sprinApi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sprinApi.domain.Categoria;
+import com.sprinApi.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Infomática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+
 	}
 }
