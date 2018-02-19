5468a9ed7d1091f549e56caf3fb4b74950376f97
Tiago Moura <trmoura@yahoo.com.br>
2018-02-19 10:58:53 -0300

    validação com bean validation em CategoriaDTO

--- src/main/java/com/sprinApi/dto/CategoriaDTO.java
+++ src/main/java/com/sprinApi/dto/CategoriaDTO.java
@@ -2,6 +2,9 @@ package com.sprinApi.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.sprinApi.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,6 +12,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 4936379629839752775L;
 
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório.")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres.")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/sprinApi/services/CategoriaService.java
+++ src/main/java/com/sprinApi/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sprinApi.domain.Categoria;
+import com.sprinApi.dto.CategoriaDTO;
 import com.sprinApi.repositories.CategoriaRepository;
 import com.sprinApi.resources.exception.DataIntegrityException;
 import com.sprinApi.resources.exception.ObjectNotFoundException;
@@ -55,9 +56,13 @@ public class CategoriaService {
 	}
 
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		
+
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
